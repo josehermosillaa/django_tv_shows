a3410055390c6b997a84813f929b11fe3b8b045a
Jose Hermosilla <jose.hermosillaasach.cl>
2021-08-20 21:53:01 -0400

    se agrega ,modelo validador

--- core/models.py
+++ core/models.py
@@ -3,6 +3,16 @@ from datetime import date, datetime
 # Create your models here.
 
 #validaciones??
+class ShowsManager(models.Manager):
+    def basic_validator(self, postData):
+        errors= {}
+        if len(postData['title'])<2:
+            errors["title"] = "Title should be at least 2 characters."
+        if len(postData['network']) <3:
+            errors["network"] = "Network should be at least 3 characters."
+        if len(postData['desc']) <10:
+            errors["desc"] = "Description must be at least 10 characters."
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
@@ -12,4 +22,4 @@ class Show(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # objects = ShowsManager()
+    objects = ShowsManager()
